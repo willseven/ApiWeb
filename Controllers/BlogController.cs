b874ade528367eda74663e42cb370b1d19ff3983
unknown <willseven>
2020-11-17 12:14:00 -0400

    updating

--- Controllers/BlogController.cs
+++ Controllers/BlogController.cs
@@ -53,6 +53,53 @@ namespace TodoApi.Controllers
 
             return createBlog;
         }
+        [HttpDelete]
+        public Blog Delete(Blog newBlog)
+        {
+             var deleteBlog = new Blog { BlogId = newBlog.BlogId };
+            // Delete
+            Console.WriteLine("Inserting a new blog");
+            _context.Remove(deleteBlog);
+            _context.SaveChanges();
+            return deleteBlog;
+        }
+        // [HttpPut]
+        // public Blog Put(Blog newBlog)
+        // {
+        //      var deleteBlog = new Blog { Url=newBlog.Url };
+        //     // Delete
+        //     Console.WriteLine("Inserting a new blog");
+        //     _context.Remove(deleteBlog);
+        //     _context.SaveChanges();
+        //     return deleteBlog;
+        // }
+
+        [HttpPut]
+        public Blog Update(Blog newBlog)
+        {
+            var blog = _context.Blogs
+                .SingleOrDefault(b=>b.BlogId == newBlog.BlogId);
+
+            blog.Url = newBlog.Url;
+            // blog.Posts.Add(
+            //         new Post
+            //         {
+                        
+            //             BlogId =  newBlog.BlogId,
+            //             Title = "Hello World",
+            //             Content = "I wrote an app using EF Core!"
+                        
+            //         });
+            var createPost = new Post { 
+                        BlogId =  newBlog.BlogId,
+                        Title = "Hello World",
+                        Content = "I wrote an app using EF Core!"
+            };
+            Console.WriteLine("updating blog");
+            _context.Update(blog);
+            _context.SaveChanges();
+            return blog;
+        } 
 
     }
 }
--- Controllers/PostController.cs
+++ Controllers/PostController.cs
@@ -53,6 +53,7 @@ namespace TodoApi.Controllers
 
             return createPost;
         }
+        
 
     }
 }
